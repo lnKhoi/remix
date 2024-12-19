573cad54b62ebeea1ac5bacef136b57885d9683d
Tan Phoi <devtanphoi@gmail.com>
2024-12-19 16:45:59 +0700

    add-skeleton-report

--- app/constants/report.constant.tsx
+++ app/constants/report.constant.tsx
@@ -1,47 +1,108 @@
 import {
   Avatar,
+  Skeleton,
   TableColumnsType,
 } from 'antd';
 import { InfluencerInReport } from '~/models/report.model';
 
-export const influencerPerformanceColumns:  TableColumnsType<InfluencerInReport> = [
-  {
-    title: 'Name',
-    render: (_, record) => <div className='flex items-center gap-3'>
-      <Avatar src={record.creator?.avatarUrl} className='w-[36px] h-[36px] rounded-[50%]' />
-      <div className='flex flex-col items-center'>
-        <p className='text-sm font-medium text-gray-800'>{record?.creator?.name}</p>
-        <p className='text-sm font-normal text-gray-500'>{record?.creator?.email}</p>
-      </div>
-    </div>
-  },
-  {
-    title: 'Engagement Rate',
-    render: (_,record) => <div className='text-sm font-normal text-gray-800'>{record?.engagementRate}%</div>
-  },
-  {
-    title: 'Revenue',
-    render: (_,record) => <div className='text-sm font-normal text-gray-800'>${record?.totalRevenue?.toFixed(2)}</div>
-  },
-  {
-    title: 'Clicks',
-    render: (_,record) => <div className='text-sm font-normal text-gray-800'>{record?.totalClicks}</div>
-  },
-  {
-    title: 'Cost Per Click (CPC)',
-    render: (_,record) => <div className='text-sm font-normal text-gray-800'>${record?.costPerClick?.toFixed(2)}</div>
-  },
-  {
-    title: 'Purchases',
-    render: (_,record) => <div className='text-sm font-normal text-gray-800'>{record?.totalOrders}</div>
-  },
-  {
-    title: 'Conversion Rate',
-    render: (_,record) => <div className='text-sm font-normal text-gray-800'>{record?.conversionRate?.toFixed(2)}%</div>
-  },
-];
+type ColumnsProps = {
+  loading: boolean
+};
 
-export const initialReport  = {
+export const influencerPerformanceColumns = ({
+  loading
+}: ColumnsProps): TableColumnsType<InfluencerInReport> => [
+    {
+      title: 'Name',
+      render: (_, record) =>
+
+        <div>
+          {
+            loading
+              ? <Skeleton.Avatar active shape='circle' />
+              :
+              <div className='flex items-center gap-3'>
+                <Avatar src={record.creator?.avatarUrl} className='w-[36px] h-[36px] rounded-[50%]' />
+                <div className='flex flex-col items-center'>
+                  <p className='text-sm font-medium text-gray-800'>{record?.creator?.name}</p>
+                  <p className='text-sm font-normal text-gray-500'>{record?.creator?.email}</p>
+                </div>
+              </div>
+          }
+        </div>
+    },
+    {
+      title: 'Engagement Rate',
+      render: (_, record) =>
+        <div>
+          {
+            loading
+              ? <Skeleton.Input active size='small' />
+              : <div className='text-sm font-normal text-gray-800'>{record?.engagementRate}%</div>
+          }
+        </div>
+    },
+    {
+      title: 'Revenue',
+      render: (_, record) =>
+        <div>
+          {
+            loading
+              ? <Skeleton.Input active size='small' />
+              : <div className='text-sm font-normal text-gray-800'>${record?.totalRevenue?.toFixed(2)}</div>
+          }
+        </div>
+
+    },
+    {
+      title: 'Clicks',
+      render: (_, record) =>
+        <div>
+          {
+            loading
+              ? <Skeleton.Input active size='small' />
+              : <div className='text-sm font-normal text-gray-800'>{record?.totalClicks}</div>
+          }
+        </div>
+    },
+    {
+      title: 'Cost Per Click (CPC)',
+      render: (_, record) =>
+        <div>
+          {
+            loading
+              ? <Skeleton.Input active size='small' />
+              : <div className='text-sm font-normal text-gray-800'>${record?.costPerClick?.toFixed(2)}</div>
+          }
+        </div>
+    },
+    {
+      title: 'Purchases',
+      render: (_, record) =>
+        <div>
+          {
+            loading
+              ? <Skeleton.Input active size='small' />
+              : <div className='text-sm font-normal text-gray-800'>{record?.totalOrders}</div>
+
+          }
+        </div>
+    },
+    {
+      title: 'Conversion Rate',
+      render: (_, record) =>
+        <div>
+          {
+            loading
+              ? <Skeleton.Input active size='small' />
+              : <div className='text-sm font-normal text-gray-800'>{record?.conversionRate?.toFixed(2)}%</div>
+          }
+        </div>
+
+    },
+  ];
+
+export const initialReport = {
   conversionRate: 0,
   totalClicks: 0,
   totalRevenue: 0,
@@ -53,8 +114,8 @@ export const initialReport  = {
   influencers: [],
   totalCost: 0,
   totalCtr: 0,
-  cpa:0,
-  totalPurchases:0
+  cpa: 0,
+  totalPurchases: 0
 }
 
 export const initialInfluencerOverLifeTime = {
