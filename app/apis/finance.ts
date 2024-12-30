165e569e39052601e6a3402026c3f14e5dc18d1d
khoilam <khoilam.dev@gmail.com>
2024-12-30 16:47:21 +0700

    pagination, search campaign

--- app/apis/finance.ts
+++ app/apis/finance.ts
@@ -1,17 +1,17 @@
 import { getData } from './axiosClient';
 
-export const getCampaignsInFinance = () => {
-    return getData(`/api/v1/finance/campaigns/financial-details`)
+export const getCampaignsInFinance = (page: number, limit: number,search:string) => {
+    return getData(`/api/v1/finance/campaigns/financial-details?page=${page}&limit=${limit}&campaignName=${search}`)
 }
 
 export const getFinanceMetrics = () => {
     return getData(`/api/v1/finance/campaigns/metrics`)
 }
 
-export const getMembersInFinance = (campaignId:string) => {
+export const getMembersInFinance = (campaignId: string) => {
     return getData(`/api/v1/finance/campaigns/${campaignId}/creators`)
 }
 
-export const getCampaignMetrics = (campaignId:string) => {
+export const getCampaignMetrics = (campaignId: string) => {
     return getData(`/api/v1/finance/campaigns/${campaignId}/metrics`)
 }
