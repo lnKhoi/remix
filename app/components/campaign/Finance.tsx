3247eb562dfa8531ca3a9897a31e95562a98c493
khoilam <khoilam.dev@gmail.com>
2025-01-14 15:19:05 +0700

    fix pagination

--- app/components/campaign/Finance.tsx
+++ app/components/campaign/Finance.tsx
@@ -1,4 +1,5 @@
 import React, {
+  ChangeEvent,
   useEffect,
   useState,
 } from 'react';
@@ -7,12 +8,17 @@ import {
   Skeleton,
   Table,
 } from 'antd';
+import debounce from 'lodash/debounce';
 import {
   getCampaignMetrics,
   getMembersInFinance,
 } from '~/apis/finance';
 import { InputSearch } from '~/components/ui/input-search';
+import ModalSelectTimeRange, {
+  DateRange,
+} from '~/components/ui/ModalSelectTimeRange';
 import { FinanceDetailsColumns } from '~/constants/finance.constant';
+import { useAuthContext } from '~/contexts/auth.context';
 import {
   FinanceMetrics,
   MemberInCampaign,
@@ -22,60 +28,92 @@ import { useParams } from '@remix-run/react';
 
 function Finance() {
     const { id } = useParams();
-
+    const { userInfo } = useAuthContext()
+  
+    const [selectedTime, setSelectedTime] = useState<{ time: string, dates: null | DateRange }>({ time: '', dates: null })
+    const [search, setSearch] = useState<string>('')
     const [loading, setLoading] = useState<boolean>(false)
+    const [loadingMetrics, setLoadingMetrics] = useState<boolean>(false)
     const [campaignMetrics, setCampaignMetric] = useState<FinanceMetrics | null>(null)
-    const [membersInFinance, setMembersInFinance] = useState<{ total: number, data: MemberInCampaign[] }>({ total: 0, data: [] })
-
+    const [membersInFinance, setMembersInFinance]
+      = useState<{ total: number, data: MemberInCampaign[] }>({ total: 0, data: [] })
+  
+    const [params, setParams] = useState<{ page: number, pageSize: number }>({ page: 1, pageSize: 10 })
+  
     const getFinanceDetails = () => {
+      if (selectedTime.time !== 'custom' || (selectedTime.dates?.[0] && selectedTime.time === 'custom')) {
         setLoading(true)
-        Promise.all([getCampaignMetrics(id as string), getMembersInFinance(id as string, '', null,'')])
-            .then(([metrics, members]) => {
-                setCampaignMetric(metrics?.data)
-                setMembersInFinance({ total: members?.data?.total, data: members?.data?.data })
-            })
-            .catch(error => { console.error('Error fetching data:', error) })
-            .finally(() => setLoading(false))
+        getMembersInFinance(id as string, selectedTime.time, selectedTime.dates, search, params.page, params.pageSize)
+          .then(res => setMembersInFinance({ total: res?.data?.total, data: res?.data?.data }))
+          .finally(() => setLoading(false))
+      }
     };
-
-    useEffect(() => { getFinanceDetails() }, [])
-
+  
+    const getFinanceMetrics = () => {
+      setLoadingMetrics(true)
+      getCampaignMetrics(id as string).then(res => setCampaignMetric(res?.data))
+        .finally(() => setLoadingMetrics(false))
+    }
+  
+    const handleSearchCampaigns = debounce((e: ChangeEvent<HTMLInputElement>): void => {
+      setSearch(e.target.value)
+      setParams({ page: 1, pageSize: 10 })
+    }, 500);
+  
+    useEffect(() => getFinanceMetrics(), [])
+    useEffect(() => { getFinanceDetails() }, [search, params,selectedTime])
+  
     return (
-        <div >
-            <div className='border border-gray-200 grid grid-cols-3 rounded-xl p-6'>
-                <div className='flex flex-col'>
-                    <p className='text-xs font-medium text-gray-800'>Member</p>
-                    <span className='text-lg font-bold text-gray-800 mt-3'>
-                        {loading ? <Skeleton.Input active size='small' /> : campaignMetrics?.totalMembers}
-                    </span>
-                </div>
-                <div className='flex flex-col'>
-                    <p className='text-xs font-medium text-gray-800'>Payment AMount</p>
-                    <span className='text-lg font-bold text-gray-800 mt-3'>
-                        {loading ? <Skeleton.Input active size='small' /> : campaignMetrics?.totalPayment.toFixed(2) + ' Tokens'}
-                    </span>
-                </div>
-                <div className='flex flex-col'>
-                    <p className='text-xs font-medium text-gray-800'>Amount Paid</p>
-                    <span className='text-lg font-bold text-gray-800 mt-3'>
-                        {loading ? <Skeleton.Input active size='small' /> : campaignMetrics?.totalPaid.toFixed(2) + ' Tokens'}
-                    </span>
-                </div>
+      <div>
+          <div className='border border-gray-200 grid grid-cols-3 rounded-xl p-6'>
+            <div className='flex flex-col'>
+              <p className='text-xs font-medium text-gray-800'>Member</p>
+              <span className='text-lg font-bold text-gray-800 mt-3'>
+                {loadingMetrics ? <Skeleton.Input active size='small' /> : campaignMetrics?.totalMembers}
+              </span>
             </div>
-
-            <div className='mt-6 flex items-center gap-3'>
-                <p className='font-medium text-base text-gray-800'>{membersInFinance?.total} Members</p>
-                <InputSearch onChange={(e) => null} placeholder='Search...' className='w-[300px] h-[36px] ' />
+            <div className='flex flex-col'>
+              <p className='text-xs font-medium text-gray-800'>Payment AMount</p>
+              <span className='text-lg font-bold text-gray-800 mt-3'>
+                {loadingMetrics ? <Skeleton.Input active size='small' /> : campaignMetrics?.totalPayment.toFixed(2) + ' Tokens'}
+              </span>
             </div>
-
-            <div className='mt-6 cursor-pointer'>
-                <Table
-                    columns={FinanceDetailsColumns(loading) as any}
-                    dataSource={loading ? Array.from({ length: 10 }, () => ({})) : membersInFinance.data}
-                />
+            <div className='flex flex-col'>
+              <p className='text-xs font-medium text-gray-800'>Amount Paid</p>
+              <span className='text-lg font-bold text-gray-800 mt-3'>
+                {loadingMetrics ? <Skeleton.Input active size='small' /> : campaignMetrics?.totalPaid.toFixed(2) + ' Tokens'}
+              </span>
             </div>
+          </div>
+  
+          <div className='mt-6 flex items-center justify-between'>
+            <div className='flex items-center gap-3'>
+              <p className='font-medium text-base text-gray-800'>{membersInFinance.total} Members</p>
+              <InputSearch onChange={(e) => handleSearchCampaigns(e)} placeholder='Search...' className='w-[300px] h-[36px] ' />
+            </div>
+            <ModalSelectTimeRange
+              allTime={`All Time`}
+              onSelect={(time, dates) => setSelectedTime({ time: time, dates: dates })}
+            />
+          </div>
+  
+          <div className='mt-6 cursor-pointer'>
+            <Table
+              pagination={{
+                pageSize: params.pageSize,
+                current: params.page,
+                total: membersInFinance.total,
+                onChange: (page, pageSize) => {
+                  setParams({ page: page, pageSize: pageSize })
+                },
+              }}
+              columns={FinanceDetailsColumns(loading) as any}
+              dataSource={loading ? Array.from({ length: 10 }, () => ({})) : membersInFinance.data}
+            />
+          </div>
         </div>
     )
-}
-
-export default Finance
+  }
+  
+  export default Finance
+  
