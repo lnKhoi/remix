ebdbdd457c4c797680f25ec2406224b5afc8ce15
khoilam <khoilam.dev@gmail.com>
2025-01-20 15:53:43 +0700

    update invite flow

--- app/components/campaign/InviteCard.tsx
+++ app/components/campaign/InviteCard.tsx
@@ -5,19 +5,15 @@ import React, {
 
 import {
   Avatar,
-  Button,
   Checkbox,
+  DatePicker,
 } from 'antd';
 import { CheckboxChangeEvent } from 'antd/es/checkbox';
-import { toast } from 'react-toastify';
-import { inviteInfluencerToCampaign } from '~/apis/campaign';
 import DefaultAvatar from '~/assets/avatar.jpeg';
-import { INVITED_INFLUENCERS } from '~/constants/messages.constant';
 import { Creator } from '~/models/User.model';
 import { formatName } from '~/utils/formatNumber';
 
-import { CheckIcon } from '@radix-ui/react-icons';
-
+import TagColor from '../ui/tagColor';
 import ModalViewInfluencerProfile from './ModalViewInfluencerProfile';
 
 type InviteCardProps = {
@@ -29,31 +25,19 @@ type InviteCardProps = {
 
 function InviteCard({ influencer, checked, onSelect, campaignId }: InviteCardProps) {
     const [invited, setInvited] = useState<boolean>(false)
-    const [loading, setLoading] = useState<boolean>(false)
     const [isModal, setIsModal] = useState<boolean>(false)
 
     const handleCheckboxChange = (e: CheckboxChangeEvent) => {
         onSelect(influencer.id as string, e.target.checked);
     };
 
-    const handleInviteInfluencer = async (): Promise<void> => {
-        setLoading(true)
-        const creatorIds = [influencer.id]
-        await inviteInfluencerToCampaign(campaignId, creatorIds as string[])
-            .then(() => {
-                setInvited(true)
-                toast.success(INVITED_INFLUENCERS)
-            })
-            .catch((err) => toast.error(err?.message))
-            .finally(() => setLoading(false))
-    }
-
     useEffect(() => {
         setInvited(influencer?.alreadyInvited as boolean)
     }, [influencer?.alreadyInvited])
 
+
     return (
-        <div className='p-4 rounded-md flex items-center justify-between border border-[#ESE7EB]'>
+        <div className='p-4 rounded-md cursor-pointer hover:border-blue-500 transition-all flex items-center justify-between border border-[#ESE7EB]'>
             <div className='w-full'>
                 <div className='flex items-start gap-4'>
                     <Checkbox
@@ -67,22 +51,17 @@ function InviteCard({ influencer, checked, onSelect, campaignId }: InviteCardPro
                                 <h5 className='text-[#1F2937] text-sm'>{formatName(influencer.name as string)}</h5>
                                 <span className='text-[#6B7280] text-sm'>{influencer.email}</span>
                             </div>
-                            {/* <div className='absolute right-0 flex items-center gap-1 top-0'>
-                                <img src={IG_ICON} alt="insta" />
-                                <span className='text-xs text-gray-800'>{influencer.followers}k</span>
-                            </div> */}
                         </div>
-                        <div >
-                            {invited
-                                ? <div className='flex h-[28px] justify-center rounded-md w-[74px] items-center  bg-[#F3F4F6]'><CheckIcon width={20} />Invited</div>
-                                : <Button loading={loading} className='h-[28px]' onClick={handleInviteInfluencer} type='primary'>Invite</Button>
+                        <div className='flex flex-col'>
+                            <div className='flex items-center'>
+                                <DatePicker style={{ maxHeight: 36 }} className='h-[36px] border-none bg-gray-100' />
+                            </div>
+                            {invited &&
+                                <div className='block'> <TagColor background='#DBEAFE' color='#1D4ED8' status='Invited' /></div>
                             }
                         </div>
                         <div className='hidden w-full  items-end border-dashed border-t border-t-gray-200 justify-between'>
                             <div className='flex items-start mt-4  h-[28px] gap-2'>
-                                {/* <Button type='text' className='bg-gray-200 h-[28px]'>Age: {influencer.age}</Button>
-                                <Button type='text' className='bg-gray-200 h-[28px]'>{influencer.gender}</Button>
-                                <Button type='text' className='bg-gray-200 h-[28px]'>{influencer.country}</Button> */}
                             </div>
                         </div>
                     </div>
--- app/components/campaign/ModalInviteInfluencerToCampaign.tsx
+++ app/components/campaign/ModalInviteInfluencerToCampaign.tsx
@@ -8,6 +8,7 @@ import React, {
 import {
   Button,
   Checkbox,
+  DatePicker,
   Modal,
   Popover,
   Radio,
@@ -15,6 +16,7 @@ import {
   Select,
   Skeleton,
   Slider,
+  Tooltip,
 } from 'antd';
 import { toast } from 'react-toastify';
 import {
@@ -31,7 +33,10 @@ import { INVITED_INFLUENCERS } from '~/constants/messages.constant';
 import { Creator } from '~/models/User.model';
 import { formatNumber } from '~/utils/formatNumber';
 
-import { AdjustmentsHorizontalIcon } from '@heroicons/react/24/outline';
+import {
+  AdjustmentsHorizontalIcon,
+  QuestionMarkCircleIcon,
+} from '@heroicons/react/24/outline';
 
 import InviteCard from './InviteCard';
 
@@ -166,95 +171,104 @@ function ModalInviteInfluencerToCampaign({ onClose, open, campaignId }: ModalInv
                                 <Checkbox checked={selectAll} onChange={handleSelectAll} >Select All</Checkbox>
                                 <span className='text-gray-600'>Selected ({selectedInfluencers.length})</span>
                             </div>
-                            <Popover
-                                content={
-                                    <div className='w-[368px] custom-input flex flex-col gap-1'>
-                                        <div>
-                                            <span className='text-sm text-gray-800 font-medium'>Age Audience</span>
+                            <div className='flex items-center gap-3'>
+                                <div className='flex items-center gap-2'>
+                                   <Tooltip title='The final date and time by which content must be submitted.'>
+                                   <QuestionMarkCircleIcon className='w-5 h-5 cursor-pointer text-gray-800' />
+                                   </Tooltip>
+                                    <span className='text-sm font-medium text-gray-800'>Deadline</span>
+                                    <DatePicker style={{ maxHeight: 36 }} className='h-[36px] bg-gray-100 border-none' />
+                                </div>
+                                <Popover
+                                    content={
+                                        <div className='w-[368px] custom-input flex flex-col gap-1'>
                                             <div>
-                                                <Radio.Group onChange={(e) => setFilter({ ...filter, age: e.target.value })}>
-                                                    <div className='grid w-[330px] mt-2 grid-cols-2 gap-2'>
-                                                        {ageAudience.map(a => (
-                                                            <Radio key={a.label} value={a.value} >{a.label}</Radio>
-                                                        ))}
-                                                    </div>
-                                                </Radio.Group>
+                                                <span className='text-sm text-gray-800 font-medium'>Age Audience</span>
+                                                <div>
+                                                    <Radio.Group onChange={(e) => setFilter({ ...filter, age: e.target.value })}>
+                                                        <div className='grid w-[330px] mt-2 grid-cols-2 gap-2'>
+                                                            {ageAudience.map(a => (
+                                                                <Radio key={a.label} value={a.value} >{a.label}</Radio>
+                                                            ))}
+                                                        </div>
+                                                    </Radio.Group>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className='mt-3'>
-                                            <span className='text-sm text-gray-800 font-medium'>Gender Audience</span>
-                                            <div className='flex items-center gap-2 mt-2'>
-                                                {genderFilterOptions.map(g => (
-                                                    <div
-                                                        onClick={() => handleSelectGender(g)}
-                                                        key={g}
-                                                        className={`h-[34px] px-3 ${!filter.gender.includes(g) ? 'bg-gray-200 text-gray-800' : 'bg-blue-600 text-white'} text-sm cursor-pointer font-medium flex items-center justify-center rounded-lg capitalize`}>
-                                                        {g}
-                                                    </div>
-                                                ))}
-                                            </div>
-                                            <div className='mt-4'>
-                                                <span className='text-sm text-gray-800 font-medium'>Social Media Platform</span>
+                                            <div className='mt-3'>
+                                                <span className='text-sm text-gray-800 font-medium'>Gender Audience</span>
                                                 <div className='flex items-center gap-2 mt-2'>
-                                                    {socials.map(s => (
+                                                    {genderFilterOptions.map(g => (
                                                         <div
-                                                            key={s.name}
-                                                            onClick={() => handleSelectSocial(s.name)}
-                                                            className={`h-[34px] px-3 text-sm cursor-pointer ${filter.socialMedias.includes(s.name) ? 'bg-blue-500 text-white' : 'bg-gray-200  text-gray-800'} 
-                                                                 font-medium flex items-center justify-center rounded-lg capitalize`}>
-                                                            {s.name}
+                                                            onClick={() => handleSelectGender(g)}
+                                                            key={g}
+                                                            className={`h-[34px] px-3 ${!filter.gender.includes(g) ? 'bg-gray-200 text-gray-800' : 'bg-blue-600 text-white'} text-sm cursor-pointer font-medium flex items-center justify-center rounded-lg capitalize`}>
+                                                            {g}
                                                         </div>
                                                     ))}
                                                 </div>
-                                            </div>
-                                            <div className='mt-[18px] flex flex-col gap-2'>
-                                                <span className='text-sm text-gray-800 font-medium'>Location Audience</span>
-                                                <Select
-                                                    placeholder="Select a country"
-                                                    value={filter.location}
-                                                    showSearch
-                                                    allowClear
-                                                    optionFilterProp="children"
-                                                    onChange={(l) => setFilter({ ...filter, location: l })}
-                                                >
-                                                    {countries.map((country) => (
-                                                        <Select.Option key={country.value} value={country.label}>
-                                                            <span role="img" aria-label={country.label} className="mr-2">
-                                                                {country.flag}
-                                                            </span>
-                                                            {country.label}
-                                                        </Select.Option>
-                                                    ))}
-                                                </Select>
-                                            </div>
-                                            <div className='mt-4'>
-                                                <span className='text-sm text-gray-800 font-medium'>Follower Count</span>
-                                                <div className='flex items-center gap-2'>
-                                                    <span className='text-xs text-gray-500'>{formatNumber(filter.minFollow as number)}</span>
-                                                    <Slider max={5000000} step={500000}
-                                                        onChange={(value: number[]) => setFilter({ ...filter, minFollow: value[0], maxFollow: value[1] })}
-                                                        range={{ draggableTrack: true }} defaultValue={[filter.minFollow as number, filter.maxFollow as number]} className='w-full' />
-                                                    <span className='text-xs text-gray-500'>{formatNumber(filter.maxFollow as number)}</span></div>
+                                                <div className='mt-4'>
+                                                    <span className='text-sm text-gray-800 font-medium'>Social Media Platform</span>
+                                                    <div className='flex items-center gap-2 mt-2'>
+                                                        {socials.map(s => (
+                                                            <div
+                                                                key={s.name}
+                                                                onClick={() => handleSelectSocial(s.name)}
+                                                                className={`h-[34px] px-3 text-sm cursor-pointer ${filter.socialMedias.includes(s.name) ? 'bg-blue-500 text-white' : 'bg-gray-200  text-gray-800'} 
+                                                                 font-medium flex items-center justify-center rounded-lg capitalize`}>
+                                                                {s.name}
+                                                            </div>
+                                                        ))}
+                                                    </div>
+                                                </div>
+                                                <div className='mt-[18px] flex flex-col gap-2'>
+                                                    <span className='text-sm text-gray-800 font-medium'>Location Audience</span>
+                                                    <Select
+                                                        placeholder="Select a country"
+                                                        value={filter.location}
+                                                        showSearch
+                                                        allowClear
+                                                        optionFilterProp="children"
+                                                        onChange={(l) => setFilter({ ...filter, location: l })}
+                                                    >
+                                                        {countries.map((country) => (
+                                                            <Select.Option key={country.value} value={country.label}>
+                                                                <span role="img" aria-label={country.label} className="mr-2">
+                                                                    {country.flag}
+                                                                </span>
+                                                                {country.label}
+                                                            </Select.Option>
+                                                        ))}
+                                                    </Select>
+                                                </div>
+                                                <div className='mt-4'>
+                                                    <span className='text-sm text-gray-800 font-medium'>Follower Count</span>
+                                                    <div className='flex items-center gap-2'>
+                                                        <span className='text-xs text-gray-500'>{formatNumber(filter.minFollow as number)}</span>
+                                                        <Slider max={5000000} step={500000}
+                                                            onChange={(value: number[]) => setFilter({ ...filter, minFollow: value[0], maxFollow: value[1] })}
+                                                            range={{ draggableTrack: true }} defaultValue={[filter.minFollow as number, filter.maxFollow as number]} className='w-full' />
+                                                        <span className='text-xs text-gray-500'>{formatNumber(filter.maxFollow as number)}</span></div>
+                                                </div>
+
+                                                <div className='flex w-full items-center gap-2 mt-5 justify-end'>
+                                                    <Button onClick={handleResetFilter} type='default' >Reset</Button>
+                                                    <Button onClick={() => { handleGetInfluencers(filter); setModal(false) }} type='primary' >Apply</Button>
+                                                </div>
                                             </div>
 
-                                            <div className='flex w-full items-center gap-2 mt-5 justify-end'>
-                                                <Button onClick={handleResetFilter} type='default' >Reset</Button>
-                                                <Button onClick={() => { handleGetInfluencers(filter); setModal(false) }} type='primary' >Apply</Button>
-                                            </div>
                                         </div>
-
-                                    </div>
-                                }
-                                title=""
-                                placement='bottom'
-                                trigger="click"
-                                open={modal}
-                                onOpenChange={() => setModal(!modal)}
-                            >
-                                <button className='bg-[#F3F4F6] hover:bg-[#D1D5DB] transition-all flex items-center justify-center gap-1 text-sm h-[35px] w-[85px] font-semibold rounded-[7px] text-[#1F2937]'>
-                                    <AdjustmentsHorizontalIcon width={16} /> Filter
-                                </button>
-                            </Popover>
+                                    }
+                                    title=""
+                                    placement='bottom'
+                                    trigger="click"
+                                    open={modal}
+                                    onOpenChange={() => setModal(!modal)}
+                                >
+                                    <button className='bg-[#F3F4F6] hover:bg-[#D1D5DB] transition-all flex items-center justify-center gap-1 text-sm h-[35px] w-[85px] font-semibold rounded-[7px] text-[#1F2937]'>
+                                        <AdjustmentsHorizontalIcon width={16} /> Filter
+                                    </button>
+                                </Popover>
+                            </div>
                         </div>
                         <div className='h-[330px] mb-8 mt-4 pr-2 overflow-y-scroll w-full flex flex-col gap-3'>
                             {loading === 'get-list'
