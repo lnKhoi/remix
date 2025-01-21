2c4859a786e50a189bbfff632816406f3e478884
khoilam <khoilam.dev@gmail.com>
2025-01-21 11:11:10 +0700

    update env

--- app/constants/env.constant.ts
+++ app/constants/env.constant.ts
@@ -1,6 +1,6 @@
 // DEV
 export const API_URL = 'https://dev.spiral.tego.global'
-export const DOMAIN  = 'https://spiral-tawny.vercel.app'
+export const DOMAIN  = 'https://lavaart-spiral-dev.vercel.app'
 export const GOOGLE_CLIENT_ID = '463266305530-ctq3htprdauqbp5qnrmkmdrcnife3mih.apps.googleusercontent.com'
 export const GOOGLE_CLIENT_TOKEN = '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'
 export const STRIPE_KEY = 'pk_test_51QUrpZQwRo0WgELJhEc1NKIjXzzfCjFgpJpF9jsBeJ0FNRJcx1x1atB2DAYjelT4C6nlcXiR9n6oYwUL8ton1dVy00EWFHCCE6'
