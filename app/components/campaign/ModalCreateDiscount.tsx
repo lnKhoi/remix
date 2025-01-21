d8ca48bf419f447b1522ebb796a95e8b1b6d1ea1
khoilam <khoilam.dev@gmail.com>
2025-01-21 12:19:11 +0700

    validation discount when create

--- app/components/campaign/ModalCreateDiscount.tsx
+++ app/components/campaign/ModalCreateDiscount.tsx
@@ -191,8 +191,8 @@ const ModalCreateDiscount = ({ products, shopId, open, onClose, onrefresh }: Mod
                         </Select>
                     </Form.Item>
 
-                    <Form.Item label="Usage Limit" name="usageLimit">
-                        <InputNumber placeholder='0' className='bg-gray-100 w-full border-none' />
+                    <Form.Item initialValue={1} label="Usage Limit" name="usageLimit">
+                        <InputNumber  placeholder='1' className='bg-gray-100 w-full border-none' />
                     </Form.Item>
                     <div className='flex pb-5 items-start'>
                         <div className='flex pb-5 items-start'>
