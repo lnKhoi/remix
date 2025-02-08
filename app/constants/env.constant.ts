fa77b1d69574f0148e34ce8350120e7ac6e210ef
khoilam <khoilam.dev@gmail.com>
2025-02-08 16:04:47 +0700

    full flow content deadline for brand

--- app/constants/env.constant.ts
+++ app/constants/env.constant.ts
@@ -3,5 +3,5 @@ export const API_URL = 'https://dev.spiral.tego.global'
 export const DOMAIN  = 'https://lavaart-spiral-dev.vercel.app'
 export const GOOGLE_CLIENT_ID = '463266305530-ctq3htprdauqbp5qnrmkmdrcnife3mih.apps.googleusercontent.com'
 export const GOOGLE_CLIENT_TOKEN = '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'
-export const STRIPE_KEY = 'pk_test_51QeoXKRuVOCHyRtNU7PLRjdRzEZiClBvbzKdAgPyaISOKWTeIvm4RV13XfnqnAlmSlBWnSiON1f2I1GoixaFELH700B0auBBy3'
+export const STRIPE_KEY = 'pk_test_51QUrpZQwRo0WgELJhEc1NKIjXzzfCjFgpJpF9jsBeJ0FNRJcx1x1atB2DAYjelT4C6nlcXiR9n6oYwUL8ton1dVy00EWFHCCE6'
 // UAT
