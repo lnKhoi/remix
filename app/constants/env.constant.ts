ee904fb5eac2576340c4c262c292bf1617ed2110
khoilam <khoilam.dev@gmail.com>
2025-03-17 15:14:11 +0700

    fix key

--- app/constants/env.constant.ts
+++ app/constants/env.constant.ts
@@ -3,5 +3,5 @@ export const API_URL = 'https://dev.spiral.tego.global'
 export const DOMAIN  = 'https://lavaart-spiral-dev.vercel.app'
 export const GOOGLE_CLIENT_ID = '463266305530-ctq3htprdauqbp5qnrmkmdrcnife3mih.apps.googleusercontent.com'
 export const GOOGLE_CLIENT_TOKEN = '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'
-export const STRIPE_KEY = 'pk_test_51QUrpZQwRo0WgELJhEc1NKIjXzzfCjFgpJpF9jsBeJ0FNRJcx1x1atB2DAYjelT4C6nlcXiR9n6oYwUL8ton1dVy00EWFHCCE6'
+export const STRIPE_KEY = 'pk_test_51QxQK7PhPEXAcbQBtw0k7JkTEIcc65isHVI5kPxj9jNY7Yrbqnob5cv5M6VfcPVVW1gF16m88EnuM2BErbLKSht000aJkcOpls'
 // UAT
